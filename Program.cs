ee8410bf19c1a489ab6dab7a102981ade0a0a772
fahminlb33 <fahminlb33@gmail.com>
2021-06-11 10:43:54 +0700

    Add examples

--- Program.cs
+++ Program.cs
@@ -21,16 +21,28 @@ namespace MigrasiLogee
                 config.SetApplicationName("migrain");
                 config.AddCommand<ScalePodsPipeline>("scale")
                     .WithDescription("Scales Kubernetes deployment replicas");
+
                 config.AddCommand<VerifyServiceUptimePipeline>("uptime")
-                    .WithDescription("Check if a service is accessible from internet using public URI");
+                    .WithDescription("Check if a service is accessible from internet using public URI")
+                    .WithExample(new []{"ingress-urls.csv", "-m", "static", "-i", "69.69.69.69"})
+                    .WithExample(new []{"ingress-urls.csv", "-m", "dynamic", "-d", "8.8.8.8"});
+                
                 config.AddCommand<VerifyDnsCutoverPipeline>("dns-propagation")
-                    .WithDescription("Check DNS propagation after Ingress configuration");
+                    .WithDescription("Check DNS propagation after Ingress configuration")
+                    .WithExample(new []{"domains.csv", "-c", "vsan-cname.playcourt.id"})
+                    .WithExample(new []{"domains.csv", "-a", "69.69.69.69"});
+
                 config.AddCommand<GetMongoDbActiveConnectionPipeline>("mongo-connection")
-                    .WithDescription("Get MongoDB active connection");
+                    .WithDescription("Get MongoDB active connection")
+                    .WithExample(new []{"logee-prod", "-p", "mongo"});
+
                 config.AddCommand<CalculateMongoDbSizePipeline>("mongo-size")
-                    .WithDescription("Calculate MongoDB database size");
+                    .WithDescription("Calculate MongoDB database size")
+                    .WithExample(new []{"logee-prod", "mongos.csv", "-p", "mongo"});
+
                 config.AddCommand<DumpMongoDbPipeline>("mongo-dump")
-                    .WithDescription("Dump all MongoDB database");
+                    .WithDescription("Dump all MongoDB database")
+                    .WithExample(new []{"logee-prod", @"D:\backup", "-p", "mongo"});
             });
 
             AnsiConsole.Render(new FigletText("MigrasiLogee").LeftAligned().Color(Color.DarkOrange3_1));
